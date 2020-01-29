2cf3545f19ba0bcbe8d66d1c9439d91454afc448
Ariel Alves Dutra <alves.lsn@gmail.com>
2020-01-29 18:17:44 -0300

    Atualiza Sobre e Adiciona MetaTags de Indexação

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -7,18 +7,9 @@ const AboutMe = () => {
     <section className="aboutMe">
       <h2 className="aboutMe__title">Sobre</h2>
       <div className="aboutMe__description">
-        <p>
-          Meu nome é Ariel Alves Dutra, tenho 24 anos e venho seguindo a carreira de desenvolvimento de software desde o final de 2017. Concluí minha graduação em agosto de 2019.
-        </p>
-        <p>
-          Eu tive duas experiências profissionais de desenvolvimento de software com PHP. Recentemente eu optei por trocar de linguagem de programação, onde eu dediquei alguns meses apenas para estudar Java, Python, React JS, CSS, HTML e outras tecnologias.
-        </p>
-        <p>
-          Eu prezo em trabalhar com colaboração, disposição para resolução de problemas, aprender constantemente, melhorar a partir de erros e experiências, praticar conteúdos técnicos periodicamente, focar no que agrega valor, argumentar, ter empatia e empenho.
-        </p>
-        <p>
-          Atualmente estou procurando oportunidades com foco em Java, NodeJS e/ou Front-End.
-        </p>
+        <p>Meu nome é Ariel Alves Dutra, tenho 24 anos e venho seguindo a carreira de desenvolvimento de software desde o final de 2017. Concluí minha graduação em agosto de 2019.</p>
+        <p>Tenho duas experiências profissionais de desenvolvimento de software com PHP. Recentemente optei por trocar de linguagem de programação, onde eu dediquei alguns meses apenas para estudar Java, Python, React JS, CSS, HTML e outras tecnologias.</p>
+        <p>Prezo em trabalhar com colaboração, disposição para resolução de problemas, aprender constantemente, melhorar a partir de erros e experiências, praticar conteúdos técnicos periodicamente, focar no que agrega valor, argumentar, ter empatia e empenho.</p>
       </div>
     </section>
   )
