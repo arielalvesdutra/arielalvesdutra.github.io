70cdd89ce4e86082527914acbbfd6f495adf0c0a
Ariel Alves Dutra <alves.lsn@gmail.com>
2024-06-05 17:58:37 -0300

    Atualiza about me

--- src/pages/about-me/AboutMe.tsx
+++ src/pages/about-me/AboutMe.tsx
@@ -10,7 +10,7 @@ const AboutMe = () => {
         <div>
           <h2>Profissional</h2>
           <p>
-            Desenvolvedor de Software com 1 ano e 4 meses de experiência profissional implementando soluções para setores de e-commerce, turismo e frete. Trabalhou com times de manutenção e de inovação, corrigindo e criando funcionalidades.
+            Desenvolvedor de Software com mais de 5 anos de experiência profissional implementando soluções para setores de e-commerce, turismo, bancário e frete. Trabalhou com times de manutenção e de inovação, corrigindo e criando funcionalidades.
           </p>
           <p>
             Principais competências incluem Desenvolvedor Backend, Desenvolvedor Frontend, Analista de Sistemas, Automatização de Processos, Agilidade e Automatização de Testes. Graduado em Análise e Desenvolvimento de Sistemas (UNISINOS).
