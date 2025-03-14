65090ef26ab2b69b4f9611d0a18e08142f9216b0
Ariel Alves Dutra <alves.lsn@gmail.com>
2025-03-14 16:24:47 -0300

    Deploy

---
+++ precache-manifest.66c4e219065b541b3c4c93fe70630ea8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "600b2e6638e3f3a4b700d9011cf6f125",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fdb223f0577f71263ad2",
+    "url": "/static/css/main.ae5d7a3f.chunk.css"
+  },
+  {
+    "revision": "d9524cd10d2096b58d25",
+    "url": "/static/js/2.0572d0fb.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.0572d0fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdb223f0577f71263ad2",
+    "url": "/static/js/main.db297f27.chunk.js"
+  },
+  {
+    "revision": "b393e4adc3a026d4d26a",
+    "url": "/static/js/runtime-main.702e1160.js"
+  }
+]);
